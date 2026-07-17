452200b807b4697f29265aa9d76a5af70930def4
Martin Vysnovsky <martinvysnovsky@gmail.com>
2026-07-17 21:41:50 +0200

    fix(kincony): modernize custom components for ESPHome 2026.x API
    Update kc868_ha switch and binary_sensor components to use the new ESPHome API:
    
    - Replace SWITCH_SCHEMA/BINARY_SENSOR_SCHEMA with switch_schema()/binary_sensor_schema()
    
    - Use switch.new_switch()/binary_sensor.new_binary_sensor() in to_code()
    
    - Remove unused CONF_ID imports and redundant declare_id() calls
    
    - Remove deprecated register_switch/register_binary_sensor calls
    
    Tested with ESPHome v2026.5.3 CLI.

--- kincony/configuration/components/kc868_ha/binary_sensor.py
+++ kincony/configuration/components/kc868_ha/binary_sensor.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
-from esphome.const import CONF_ID
 from . import Kc868HaComponent, KC868_HA_ID
 
 DEPENDENCIES = ['kc868_ha']
@@ -9,8 +8,7 @@ DEPENDENCIES = ['kc868_ha']
 kc868_ha_ns = cg.esphome_ns.namespace('kc868_ha')
 BinarySensor = kc868_ha_ns.class_('KC868HaBinarySensor', cg.Component, binary_sensor.BinarySensor)
 
-CONFIG_SCHEMA = binary_sensor.BINARY_SENSOR_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(BinarySensor),
+CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(BinarySensor).extend({
     cv.GenerateID(KC868_HA_ID): cv.use_id(Kc868HaComponent),
     cv.Optional("target_relay_controller_addr", default=1): cv.int_range(min=1, max=128),
     cv.Optional("switch_adapter_addr", default=10): cv.int_range(min=10, max=99),
@@ -19,9 +17,8 @@ CONFIG_SCHEMA = binary_sensor.BINARY_SENSOR_SCHEMA.extend({
 
 def to_code(config):
     paren = yield cg.get_variable(config[KC868_HA_ID])
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = yield binary_sensor.new_binary_sensor(config)
     yield cg.register_component(var, config)
-    yield binary_sensor.register_binary_sensor(var, config)
     cg.add(var.set_target_relay_controller_addr(config["target_relay_controller_addr"]))
     cg.add(var.set_switch_adapter_addr(config["switch_adapter_addr"]))
     cg.add(var.set_bind_output(config["bind_output"]))
